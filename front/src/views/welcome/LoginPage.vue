b9c08104142717b3c1bf11ff01bd2c4c8ce6ad95
swn233 <swndaze@gmail.com>
2023-12-07 16:51:11 +0800

    开始二次开发

---
+++ front/src/views/welcome/LoginPage.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+<div style="text-align: center;margin:0 20px">
+  <div style="margin-top: 150px">
+      <div style="font-size: 28px;font-weight: bold">登陆</div>
+      <div></div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
